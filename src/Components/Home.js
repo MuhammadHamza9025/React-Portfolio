1496a9df105009e7c278425623e8f8f4582da77f
Muhammad Hamza <139134628+MuhammadHamza9025@users.noreply.github.com>
2023-08-27 15:49:13 +0500

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -39,17 +39,24 @@ const Home = () => {
         wrapper="a"
       />
     </div>
-    <div className="para"  ><p>Experienced front-end developer adept at crafting responsive and visually engaging web interfaces, utilizing
+    <div className="para"  data-aos='fade-up'><p>Experienced front-end developer adept at crafting responsive and visually engaging web interfaces, utilizing
    
    HTML, CSS, JavaScript and ReactJs to deliver seamless user experiences. Proficient in translating design
-   concepts into functional code, with a strong focus on usability and accessibility.</p></div>
+   concepts into functional code, with a strong focus on usability and accessibility.</p> 
+   <div className="rightcont2" data-aos='fade-up'>
+<a href="https://www.facebook.com/profile.php?id=100074954437529" className="fa fa-facebook"  data-aos="fade-zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="600" target='blank'></a>
+<a href="https://github.com/MuhammadHamza9025" className="fa fa-github" data-aos="flip-up"  target='blank'></a>
+<a href="#" className="fa fa-google" data-aos="fade-up"></a>
+<a href="https://www.linkedin.com/in/muhammad-hamza-586119226/" className="fa fa-linkedin" data-aos="fade-up" target='blank'></a>
+<span></span>
+</div></div>
   </div>
  </div></div>
 <div className="rightcont">
-<a href="https://www.facebook.com/profile.php?id=100074954437529" class="fa fa-facebook"  data-aos="fade-zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="600" target='blank'></a>
-<a href="https://github.com/MuhammadHamza9025" class="fa fa-github" data-aos="flip-up"  target='blank'></a>
-<a href="#" class="fa fa-google" data-aos="fade-up"></a>
-<a href="https://www.linkedin.com/in/muhammad-hamza-586119226/" class="fa fa-linkedin" data-aos="fade-up" target='blank'></a>
+<a href="https://www.facebook.com/profile.php?id=100074954437529" className="fa fa-facebook"  data-aos="fade-zoom-in" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="600" target='blank'></a>
+<a href="https://github.com/MuhammadHamza9025" className="fa fa-github" data-aos="fade-up"  target='blank'></a>
+<a href="#" className="fa fa-google" data-aos="fade-up"></a>
+<a href="https://www.linkedin.com/in/muhammad-hamza-586119226/" className="fa fa-linkedin" data-aos="fade-up" target='blank'></a>
 <span></span>
 </div>
  </div></div>
